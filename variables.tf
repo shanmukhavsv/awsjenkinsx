b97b9af1888aa6314022274992830cce08cbb0ac
shanmukhavsv <ishanmukhayadav@gmail.com>
2022-03-14 10:52:12 +0530

    fix: configure cluster repository and project2

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@
 variable "region" {
   description = "AWS region code for creating resources."
   type        = string
-  default     = eu-west-1
+  value       = eu-west-1
 }
 
 variable "profile" {
@@ -15,7 +15,7 @@ variable "profile" {
 variable "cluster_version" {
   description = "Kubernetes version to use for the EKS cluster."
   type        = string
-  default     = 1.20
+  value       = 1.20
 }
 
 variable "vault_user" {
@@ -45,13 +45,13 @@ variable "is_jx2" {
 variable "jx_git_url" {
   description = "URL for the Jenins X cluster git repository"
   type        = string
-  default     = https://github.com/shanmukhavsv/awsjenkinsx.git
+  value       = https://github.com/shanmukhavsv/awsjenkinsx.git
 }
 
 variable "jx_bot_username" {
   description = "Bot username used to interact with the Jenkins X cluster git repository"
   type        = string
-  default     = ishanmukhayadav
+  value       = ishanmukhayadav
 }
 
 variable "jx_bot_token" {
